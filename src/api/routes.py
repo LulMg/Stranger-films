703ca15172bc76ffa7dcbd0ef6ae90ee36487c7a
Chris-Durr <chrisdurr88@gmail.com>
2022-07-03 11:16:58 +0000

    test SQL

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Comment
+from api.models import db, User, Comment, Fav_movie, List_movie
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
